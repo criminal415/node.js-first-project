275f46f4963338133967c39119aa24c3e2a70cb1
node.js-first-project <denny415@naver.com>
2021-10-10 04:21:39 +0900

    ejs file 코드 정리중

--- public/api.js
+++ public/api.js
@@ -1,3 +1,8 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const _id = urlParams.get("_id");
+const keyword = urlParams.get("keyword");
+
 function signOut() {
   localStorage.clear();
   window.location.href = '/';
@@ -35,4 +40,77 @@ function loginState() {
     $('.login').css('display', 'block');
     $('.logout').css('display', 'none');
   }
+}
+
+function correct_detail() {
+  $.ajax({
+      type: "GET",
+      url: `/api/posts/correction/${_id}`,
+      headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      data: {},
+      error: function (xhr, status, error) {
+          if (status == 401) {
+              alert('로그인이 필요합니다.')
+          }
+          window.location.href = "/";
+      },                
+      success: function (response) {
+          let post = response["post"];
+          
+          $("#title").val(post["title"]);
+          $("#content").text(post["content"]);
+      }
+  });
+}
+
+function correction() {
+  $.ajax({
+      type: "PATCH",
+      url: `/api/posts/correction/${_id}`,
+      headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      data: {
+          title: $("#title").val(),
+          content: $("#content").val(),
+      },
+      error: function (xhr, status, error) {
+          if (status == 401) {
+              alert('로그인이 필요합니다.')
+          }
+          window.location.href = "/";
+      },
+      success: function (response) {
+          if (response["result"] == "success") {
+              alert("변경 완료!");
+              window.location.href = "/";
+          }
+      }
+  });
+}
+
+function delete_post() {
+
+  $.ajax({
+      type: "DELETE",
+      url: `/api/posts/delete/${_id}`,
+      headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      data: {},
+      error: function (xhr, status, error) {
+          if (status == 401) {
+              alert('로그인이 필요합니다.')
+          }
+          window.location.href = "/";
+      },
+      success: function (response) {
+          if (response["result"] == "success") {
+              alert("삭제 완료!");
+              window.location.href = "/";
+          }
+      }
+  });
 }
